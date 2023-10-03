a2c6580f057e214de03dd52c17166c24270a482f
[name] <nitinpatel9860@gmail.com>
2023-10-03 21:54:37 +0530

    'setup'

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        lightBlue: "#404EED",
+      },
+      fontFamily: {
+        Whitney: ["Whitney"],
+        Ginto: ["Ginto"],
+        ggSans: ["ggSans"],
+      },
+    },
+  },
+  plugins: [],
+};
